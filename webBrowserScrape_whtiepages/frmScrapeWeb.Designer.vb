b4efe23839079d0ea7e4f524e651915e8b1dbfe2
Shivam <soodshivam1831997@gmail.com>
2020-04-15 14:41:49 -0400

    Initial Commit

---
+++ webBrowserScrape_whtiepages/frmScrapeWeb.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmScrapeWeb
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmScrapeWeb))
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.btnScrape = New System.Windows.Forms.ToolStripSplitButton()
+        Me.lblURL = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnLoadURL = New System.Windows.Forms.ToolStripSplitButton()
+        Me.btnLoadURLList = New System.Windows.Forms.ToolStripSplitButton()
+        Me.lblStats = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.wb = New System.Windows.Forms.WebBrowser()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnScrape, Me.lblURL, Me.btnLoadURL, Me.btnLoadURLList, Me.lblStats})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 473)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Padding = New System.Windows.Forms.Padding(1, 0, 9, 0)
+        Me.StatusStrip1.Size = New System.Drawing.Size(699, 22)
+        Me.StatusStrip1.TabIndex = 7
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'btnScrape
+        '
+        Me.btnScrape.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.btnScrape.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.btnScrape.Image = CType(resources.GetObject("btnScrape.Image"), System.Drawing.Image)
+        Me.btnScrape.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnScrape.Name = "btnScrape"
+        Me.btnScrape.Size = New System.Drawing.Size(58, 20)
+        Me.btnScrape.Text = "Scrape"
+        '
+        'lblURL
+        '
+        Me.lblURL.Name = "lblURL"
+        Me.lblURL.Size = New System.Drawing.Size(21, 17)
+        Me.lblURL.Text = "url"
+        '
+        'btnLoadURL
+        '
+        Me.btnLoadURL.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.btnLoadURL.Image = CType(resources.GetObject("btnLoadURL.Image"), System.Drawing.Image)
+        Me.btnLoadURL.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnLoadURL.Name = "btnLoadURL"
+        Me.btnLoadURL.Size = New System.Drawing.Size(73, 20)
+        Me.btnLoadURL.Text = "Load URL"
+        Me.btnLoadURL.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        '
+        'btnLoadURLList
+        '
+        Me.btnLoadURLList.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.btnLoadURLList.Image = CType(resources.GetObject("btnLoadURLList.Image"), System.Drawing.Image)
+        Me.btnLoadURLList.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnLoadURLList.Name = "btnLoadURLList"
+        Me.btnLoadURLList.Size = New System.Drawing.Size(84, 20)
+        Me.btnLoadURLList.Text = "Load url list"
+        Me.btnLoadURLList.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage
+        '
+        'lblStats
+        '
+        Me.lblStats.Name = "lblStats"
+        Me.lblStats.Size = New System.Drawing.Size(36, 17)
+        Me.lblStats.Text = "# urls"
+        '
+        'wb
+        '
+        Me.wb.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wb.Location = New System.Drawing.Point(0, 0)
+        Me.wb.Margin = New System.Windows.Forms.Padding(2)
+        Me.wb.MinimumSize = New System.Drawing.Size(13, 13)
+        Me.wb.Name = "wb"
+        Me.wb.Size = New System.Drawing.Size(699, 495)
+        Me.wb.TabIndex = 6
+        '
+        'frmScrapeWeb
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(699, 495)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.wb)
+        Me.Name = "frmScrapeWeb"
+        Me.Text = "Scrape Web"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents btnScrape As ToolStripSplitButton
+    Friend WithEvents lblURL As ToolStripStatusLabel
+    Friend WithEvents btnLoadURL As ToolStripSplitButton
+    Friend WithEvents wb As WebBrowser
+    Friend WithEvents btnLoadURLList As ToolStripSplitButton
+    Friend WithEvents lblStats As ToolStripStatusLabel
+End Class
---
+++ webBrowserScrape_whtiepages/frmScrapeWeb.vb
@@ -0,0 +1,160 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text.RegularExpressions
+Imports System.Threading
+Imports System.Timers
+
+Public Class frmScrapeWeb
+#Region "Global"
+    Private Shared dir As String = Regex.Replace(My.Application.Info.DirectoryPath, "\bbin\b\\.*", "") ' .Replace("bin\Debug", "")
+    Public Shared appData As String = String.Format("{0}appData\{1}", dir, "{0}")
+    Public Shared scrapedDir As String = String.Format(appData, "scraped\{0}")
+
+    Private urlList As New List(Of String)
+    Private url As String = ""
+    Private ttl As Double = 0
+#End Region
+#Region "Browser Simulation"
+
+
+    Private Const BrowserKeyPath As String = "\SOFTWARE\Microsoft\Internet Explorer\MAIN\FeatureControl\FEATURE_BROWSER_EMULATION"
+    Private Sub CreateBrowserKey(Optional ByVal IgnoreIDocDirective As Boolean = False)
+        Dim basekey As String = Microsoft.Win32.Registry.CurrentUser.ToString
+        Dim value As Int32
+        Dim thisAppsName As String = My.Application.Info.AssemblyName & ".exe"
+        ' Value reference: http://msdn.microsoft.com/en-us/library/ee330730%28v=VS.85%29.aspx
+        ' IDOC Reference:  http://msdn.microsoft.com/en-us/library/ms535242%28v=vs.85%29.aspx
+        Select Case (New WebBrowser).Version.Major
+            Case 8
+                If IgnoreIDocDirective Then
+                    value = 8888
+                Else
+                    value = 8000
+                End If
+            Case 9
+                If IgnoreIDocDirective Then
+                    value = 9999
+                Else
+                    value = 9000
+                End If
+            Case 10
+                If IgnoreIDocDirective Then
+                    value = 10001
+                Else
+                    value = 10000
+                End If
+            Case 11
+                If IgnoreIDocDirective Then
+                    value = 11001
+                Else
+                    value = 11000
+                End If
+
+            Case Else
+                Exit Sub
+        End Select
+        Microsoft.Win32.Registry.SetValue(Microsoft.Win32.Registry.CurrentUser.ToString & BrowserKeyPath,
+                                          Process.GetCurrentProcess.ProcessName & ".exe",
+                                          value,
+                                          Microsoft.Win32.RegistryValueKind.DWord)
+    End Sub
+    Private Sub RemoveBrowerKey()
+        Dim key As Microsoft.Win32.RegistryKey
+        key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(BrowserKeyPath.Substring(1), True)
+        key.DeleteValue(Process.GetCurrentProcess.ProcessName & ".exe", False)
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+        RemoveBrowerKey()
+    End Sub
+#End Region
+
+    Public Sub New()
+        CreateBrowserKey()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        wb.ScriptErrorsSuppressed = True
+    End Sub
+
+    Private Sub btnLoadURL_ButtonClick(sender As Object, e As EventArgs) Handles btnLoadURL.ButtonClick
+        If url = "" Then
+            MsgBox("There are currently no urls to view", MsgBoxStyle.Critical)
+        Else
+            lblURL.Text = url
+            wb.Navigate(url)
+        End If
+    End Sub
+
+    Private Sub btnScrape_ButtonClick(sender As Object, e As EventArgs) Handles btnScrape.ButtonClick
+        AddHandler t.Elapsed, New ElapsedEventHandler(AddressOf Elapsed)
+        t.Start()
+    End Sub
+
+    Private Sub btnLoadURLList_ButtonClick(sender As Object, e As EventArgs) Handles btnLoadURLList.ButtonClick
+        Using dr As New StreamReader(String.Format(appData, "url.txt"))
+            Do Until dr.EndOfStream
+                urlList.Add(dr.ReadLine)
+            Loop
+        End Using
+
+        ttl = urlList.Count
+
+        Using dr As New StreamReader(String.Format(appData, "complete.txt"))
+            Do Until dr.EndOfStream
+                Dim tmp_url As String = dr.ReadLine.Split(",")(1)
+                urlList.Remove(tmp_url)
+            Loop
+        End Using
+
+        url = urlList(0)
+        urlList.RemoveAt(0)
+
+        lblURL.Text = url
+        lblStats.Text = String.Format("Remaining:{0}/{1}", urlList.Count + 1, ttl)
+    End Sub
+
+    Dim bool As Boolean = False
+
+    Private t As New Timers.Timer(5000)
+    Private Sub Elapsed(ByVal sender As Object, ByVal e As ElapsedEventArgs)
+        Try
+            Me.Invoke(New MethodInvoker(AddressOf extractNextURL))
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Public Sub extractNextURL()
+        Dim txt As String = wb.Document.Body.InnerHtml
+
+        Dim id As Guid = Guid.NewGuid
+
+        Using wr As New StreamWriter(String.Format(scrapedDir, id.ToString & ".txt"))
+            wr.AutoFlush = True
+            wr.Write(txt)
+            wr.Close()
+        End Using
+
+        Using wr As New StreamWriter(String.Format(appData, "complete.txt"), append:=True)
+            wr.AutoFlush = True
+            wr.WriteLine(String.Format("{0},{1}", id.ToString, url.ToString))
+            wr.Close()
+        End Using
+
+        If urlList.Count = 0 Then
+            t.Stop()
+            MsgBox("Complete!!!", MsgBoxStyle.Critical)
+        End If
+
+        url = urlList(0)
+        urlList.RemoveAt(0)
+
+        lblURL.Text = url
+        lblStats.Text = String.Format("Remaining:{0}/{1}", urlList.Count + 1, ttl)
+
+        wb.Navigate(url)
+    End Sub
+End Class
